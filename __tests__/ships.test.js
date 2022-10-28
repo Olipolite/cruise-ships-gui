ec2da27132d89a0493a6d051ab5d36929764ab77
Olipolite <oliver.oquist3d@gmail.com>
2022-10-28 22:34:08 +0100

    Added setSail method and test

--- __tests__/ships.test.js
+++ __tests__/ships.test.js
@@ -1,13 +1,21 @@
-const Ship = require("../src/ships")
+const Ship = require("../src/ships");
 
 describe("constructor", () => {
-    it("returns an object", () => {
-        expect(new Ship("shipTom")).toBeInstanceOf(Object);
-    });
+  it("returns an object", () => {
+    expect(new Ship("shipTom")).toBeInstanceOf(Object);
+  });
 
-    it("returns a starting point for the ship", () => {
-        const ship = new Ship("shipTom")
+  it("returns a starting point for the ship", () => {
+    const ship = new Ship("shipTom");
 
-        expect(ship.startingPort).toEqual("Stockholm");
-    })
+    expect(ship.startingPort).toEqual("Stockholm");
+  });
+
+  it("sets sail from startingPort", () => {
+    const ship = new Ship("shipTom");
+
+    ship.setSail();
+
+    expect(ship.startingPort).toBeFalsy();
+  });
 });
--- src/ships.js
+++ src/ships.js
@@ -1,8 +1,18 @@
-const ship = new Ship("shipTom")
+const ship = new Ship("shipTom");
 
 function Ship(name) {
-    this.name = name;
-    this.startingPort = "Stockholm";
+  this.name = name;
+  this.startingPort = "Stockholm";
+}
+
+Ship.prototype.setSail = function () {
+  return (this.startingPort = false);
 };
+//class Ship {
+//constructor(name) {
+//this.name = name;
+//this.startingPort = "Stockholm";
+//}
+//}
 
 module.exports = Ship;
