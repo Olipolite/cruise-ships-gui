0be8ce038b026266ece815f27b6b4d8d76ee8262
Olipolite <oliver.oquist3d@gmail.com>
2022-11-05 11:12:13 +0000

    Did some DRYing to the Ships and Test suits. Added beforeEach.

--- __tests__/port.test.js
+++ __tests__/port.test.js
@@ -2,11 +2,7 @@ const Port = require("../src/port");
 const Ship = require("../src/ships")
 const Itinerary = require("../src/itinerary")
 
-describe("constructorPort", () => {
-  it("returns the object port", () => {
-    expect(new Port()).toBeInstanceOf(Object);
-  });
-
+describe("Port name", () => {
   it("returns the name of the port", () => {
     const port = new Port("Helsinki");
 
@@ -15,26 +11,32 @@ describe("constructorPort", () => {
 });
 
 describe("Port methods ",() => {
+  let port
+  let titanic;
+  let queenMary;
+  beforeEach(() => {
+    port = new Port("Stockholm")
+    titanic = {};
+    queenMary = {}
+  });
+
+  it("returns the object port", () => {
+    expect(new Port()).toBeInstanceOf(Object);
+  });
+
   it("adds ships to Port", () => {
-    const port = new Port("Stockholm");
-    const ship = {};
-    const knark = {};
-
-    port.addShip(ship);
-    port.addShip(knark);
-
-    expect(port.ships).toContain(ship, knark);
-  })
+    port.addShip(titanic);
+    port.addShip(queenMary);
+  
+    expect(port.ships).toContain(titanic, queenMary);
+  });
+  
 
   it("remove ships from Port", () => {
-    const port = new Port("Stockholm");
-    const titanic = {};
-    const queenMary = {};
-
     port.addShip(titanic);
     port.addShip(queenMary);
     port.removeShip(queenMary);
 
     expect(port.ships).toEqual([titanic]);
-  })
+  });
 });
--- __tests__/ships.test.js
+++ __tests__/ships.test.js
@@ -2,43 +2,40 @@ const Ship = require("../src/ships");
 const Port = require("../src/port");
 const Itinerary = require("../src/itinerary");
 
-describe("constructor", () => {
-  it("returns an object", () => {
-    const port = new Port("Stockholm");
-    const itinerary = new Itinerary([port]);
-    const ship = new Ship(itinerary);
+describe("Ship", () => {
+  describe("with ports and an itinerary", () => {
+    let ship;
+    let stockholm;
+    let helsinki;
+    let itinerary;
 
-    expect(ship).toBeInstanceOf(Object);
+    beforeEach(() => {
+      stockholm = new Port("Stockholm");
+      helsinki = new Port("Helsinki");
+      itinerary = new Itinerary([stockholm, helsinki]);
+      ship = new Ship(itinerary);
+    });
+
+    it("can be instantiated", () => {
+      expect(ship).toBeInstanceOf(Object);
+    });
+  
+    it("gets added to port on instantiation", () => {
+      expect(stockholm.ships).toContain(ship);
+    });
+  
+    it("has a starting port", () => {
+      expect(ship.currentPort).toBe(stockholm);
+    });
+  
+    it("sets sail from currentPort", () => {
+      ship.setSail();
+  
+      expect(ship.currentPort).toBeFalsy();
+      expect(stockholm.ships).not.toContain(ship);
+    });
   });
-
-  it("gets added to port on instantiation", () => {
-    const stockholm = new Port("Stockholm");
-    const itinerary = new Itinerary([stockholm]);
-    const ship = new Ship(itinerary);
-
-    expect(stockholm.ships).toContain(ship);
-  })
-
-  it("a starting port for the ship", () => {
-    const port = new Port("Stockholm");
-    const itinerary = new Itinerary([port]);
-    const ship = new Ship(itinerary);
-
-    expect(ship.currentPort).toBe(port);
-  });
-
-  it("sets sail from currentPort", () => {
-    const stockholm = new Port("Stockholm");
-    const helsinki = new Port("Helsinki");
-    const itinerary = new Itinerary([stockholm, helsinki]);
-    const ship = new Ship(itinerary);
-
-    ship.setSail();
-
-    expect(ship.currentPort).toBeFalsy();
-    expect(stockholm.ships).not.toContain(ship);
-  });
-
+  
   it("can dock at a different port", () => {
     const stockholm = new Port("Stockholm");
     const helsinki = new Port("Helsinki");
@@ -64,3 +61,4 @@ describe("constructor", () => {
     expect(() => ship.setSail()).toThrowError("End of itinerary reached");
   });
 });
+  
