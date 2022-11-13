9627f96a90ad4f472aed444d5a415fe4cd6cfb9b
Olipolite <oliver.oquist3d@gmail.com>
2022-11-13 19:26:49 +0000

    Added new prototype.setSail. Also a setInterval method to set the speed of the ship.

--- src/controller.js
+++ src/controller.js
@@ -1,6 +1,12 @@
 (function exportController() {
-  function Controller() {
+  function Controller(ship) {
+    this.ship = ship;
+
     this.initialiseSea();
+
+    document.querySelector("#sailbutton").addEventListener("click", () => {
+      this.setSail();
+    });
   }
 
   Controller.prototype.initialiseSea = function initialiseSea() {
@@ -30,7 +36,9 @@
     });
   };
 
-  Controller.prototype.renderShip = function renderShip(ship) {
+  Controller.prototype.renderShip = function renderShip() {
+    const ship = this.ship;
+
     const shipPortIndex = ship.itinerary.ports.indexOf(ship.currentPort);
     const portElement = document.querySelector(
       `[data-port-index="${shipPortIndex}"]`
@@ -38,6 +46,32 @@
     const shipElement = document.querySelector("#ship");
     shipElement.style.top = `${portElement.offsetTop + 32}px`;
     shipElement.style.left = `${portElement.offsetLeft - 32}px`;
+  };
+
+  Controller.prototype.setSail = function setSail() {
+    const ship = this.ship;
+
+    const currentPortIndex = ship.itinerary.ports.indexOf(ship.currentPort);
+    const nextPortIndex = currentPortIndex + 1;
+    const nextPortElement = document.querySelector(
+      `[data-port-index="${nextPortIndex}"]`
+    );
+
+    if (!nextPortElement) {
+      return alert("End of the line!");
+    }
+
+    const shipElement = document.querySelector("#ship");
+    const sailInterval = setInterval(() => {
+      const shipLeft = parseInt(shipElement.style.left, 10);
+      if (shipLeft === nextPortElement.offsetLeft - 32) {
+        ship.setSail();
+        ship.dock();
+        clearInterval(sailInterval);
+      }
+
+      shipElement.style.left = `${shipLeft + 1}px`;
+    }, 20);
   };
 
   if (typeof module !== "undefined" && module.exports) {
